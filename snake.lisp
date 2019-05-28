1e9bc08fcfe8df6a4aa9f5c015780e76ddc9e6ee
Brad Svercl <bradsvercl@gmail.com>
2019-05-28 09:48:52 -0500

    Documentation change

--- snake.lisp
+++ snake.lisp
@@ -20,7 +20,7 @@
           (gamekit:y b))))
 
 (defun mod-vec (this that)
-  "MOD x and y of VEC by DIVISOR."
+  "MOD x and y of THIS by x and y of THAT."
   (gamekit:vec2 (mod (gamekit:x this) (gamekit:x that))
                 (mod (gamekit:y this) (gamekit:y that))))
 
