7032a18c11fb213d0bf825f0c536d4512d8f45e1
Brad Svercl <bradsvercl@gmail.com>
2019-05-27 22:54:34 -0500

    Hey, that's pretty good!

--- snake.lisp
+++ snake.lisp
@@ -5,10 +5,12 @@
 (defparameter +segment-size+ 25.0)
 (defparameter +screen-width+ 800)
 (defparameter +screen-height+ 600)
+(defparameter +segments-across-width+ (floor (/ +screen-width+ +segment-size+)))
+(defparameter +segments-across-height+ (floor (/ +screen-height+ +segment-size+)))
 
 (defparameter +snake-color+ (gamekit:vec4 1.0 0.75 0.5 1.0))
 (defparameter +food-color+ (gamekit:vec4 0.5 0.25 1.0 1.0))
-(defparameter +grid-color+ (gamekit:vec4 0.8 0.8 0.8 1.0))
+(defparameter +grid-color+ (gamekit:vec4 0.9 0.9 0.9 0.5))
 (defparameter +transparent+ (gamekit:vec4))
 
 (defun vec2= (a b)
@@ -20,17 +22,8 @@
 ;; TODO(bsvercl): Use this for wrapping snake position.
 (defun mod-vec (vec divisor)
   "MOD x and y of VEC by DIVISOR."
-  (gamekit:vec2 (mod (gamekit:x vec) divisor)
-                (mod (gamekit:y vec) divisor)))
-
-(defun floor-vec (vec)
-  "FLOOR x and y of VEC."
-  (gamekit:vec2 (floor (gamekit:x vec))
-                (floor (gamekit:y vec))))
-
-(defun snap-to-grid (vec &optional (cell-size (gamekit:vec2 +segment-size+ +segment-size+)))
-  "Snap VEC to CELL-SIZE."
-  (gamekit:mult (floor-vec (gamekit:div vec cell-size)) cell-size))
+  (gamekit:vec2 (mod (gamekit:x vec) (gamekit:x divisor))
+                (mod (gamekit:y vec) (gamekit:y divisor))))
 
 (defclass snake ()
   ((segments :initarg :segments :accessor segments-of)
@@ -43,11 +36,13 @@
     (make-instance 'snake :segments segments :direction direction)))
 
 (defun snake-position (snake)
+  "Head position of SNAKE."
   (first (segments-of snake)))
 
 (defmethod (setf snake-position) (pos snake)
   (setf (first (segments-of snake)) pos))
 
+;; TODO(bsvercl): Don't allow Left<->Right Up<->Down
 (defun change-direction (snake new-direction)
   "Modify DIRECTION of SNAKE with NEW-DIRECTION."
   (setf (direction-of snake) new-direction))
@@ -55,35 +50,32 @@
 (defun advance (snake ate-food-p)
   (with-slots (segments direction) snake
     (let* ((position (snake-position snake))
-           (new-head (gamekit:add position (gamekit:mult direction +segment-size+)))
-           (segments (if ate-food-p segments (butlast segments)))
-           (new-segments (push new-head segments)))
-      (mapc #'(lambda (pos) (gamekit:add pos (gamekit:mult direction +segment-size+))) segments)
+           (head (gamekit:add position  direction))
+           (which-segments (if ate-food-p segments (butlast segments)))
+           (new-segments (push head which-segments)))
+      (mapc #'(lambda (pos) (gamekit:add pos direction)) segments)
       (setf segments new-segments)
-      (cond
-        ((> (gamekit:x position) +screen-width+)
-         (setf (gamekit:x (snake-position snake)) 0))
-        ((< (gamekit:x position) 0)
-         (setf (gamekit:x (snake-position snake)) +screen-width+))
-        ((> (gamekit:y (snake-position snake)) +screen-height+)
-         (setf (gamekit:y (snake-position snake)) 0))
-        ((< (gamekit:y (snake-position snake)) 0)
-         (setf (gamekit:y (snake-position snake)) +screen-height+))))))
+      ;; Wrap SNAKE around the boundaries
+      (setf (snake-position snake) (mod-vec (snake-position snake)
+                                            (gamekit:vec2 +segments-across-width+
+                                                          +segments-across-height+))))))
 
 (gamekit:defgame snake-game ()
   ((player :reader player-of)
-   (food-pos :accessor food-pos-of))
+   (food-pos :accessor food-pos-of)
+   (score :initform 0 :reader score-of))
   (:viewport-title "Snake")
   (:viewport-width +screen-width+)
   (:viewport-height +screen-height+))
 
 (defun new-food-pos ()
-  (snap-to-grid (gamekit:vec2 (random +screen-width+)
-                              (random +screen-height+))))
+  "Generates a new spot on the grid."
+  (gamekit:vec2 (random +segments-across-width+)
+                (random +segments-across-height+)))
 
 (defmethod gamekit:post-initialize ((this snake-game))
   (with-slots (player food-pos) this
-    (setf player (make-snake (gamekit:vec2)))
+    (setf player (make-snake (gamekit:vec2 (/ +segments-across-width+ 2) (/ +segments-across-height+ 2))))
     ;; TODO(bsvercl): avoid selecting a spot occupied by the player.
     (setf food-pos (new-food-pos))
     ;; TODO(bsvercl): ??? I think we can go deeper.
@@ -100,42 +92,44 @@
       (binder :s (change-direction player (gamekit:vec2 0 -1)))
       (binder :a (change-direction player (gamekit:vec2 -1 0)))
       (binder :d (change-direction player (gamekit:vec2 1 0)))
-      (binder :space (change-direction player (gamekit:vec2)))
       ;; NOTE: This is just for debugging.
+      (binder :space (change-direction player (gamekit:vec2)))
+      (binder :e (setf (snake-position player) (gamekit:vec2)))
       (binder :q (setf food-pos (new-food-pos))))))
 
 ;; TODO(bsvercl): Game moves too fast.
 (defmethod gamekit:act ((this snake-game))
-  (with-slots (player food-pos) this
+  (with-slots (player food-pos score) this
     (let* ((position (snake-position player))
            (ate-food-p (vec2= food-pos position)))
       (advance player ate-food-p)
       (when ate-food-p
-        (setf food-pos (new-food-pos)))))
+        (setf food-pos (new-food-pos))
+        (incf score 100))))
   ;; TODO(bsvercl): Remove this SLEEP.
-  (sleep 0.1))
+  (sleep 0.09))
 
 (defmethod gamekit:draw ((this snake-game))
   ;; TODO(bsvercl): Drop into CL-BODGE to speed this up.
-  (loop for x from 0 to (floor (/ +screen-width+ +segment-size+))
-        for xx = (floor (* x +segment-size+))
-        do (loop for y from 0 to (floor (/ +screen-height+ +segment-size+))
-                 for yy = (* y +segment-size+)
-                 do (gamekit:draw-rect (gamekit:vec2 xx yy)
-                                       +segment-size+
-                                       +segment-size+
+  (loop for x from 0 below +segments-across-width+
+        do (loop for y from 0 below +segments-across-height+
+                 do (gamekit:draw-rect (gamekit:mult (gamekit:vec2 x y) +segment-size+)
+                                       +segment-size+ +segment-size+
                                        :fill-paint +transparent+
                                        :stroke-paint +grid-color+)))
+ ;; Draw food
+  (gamekit:draw-rect (gamekit:mult (food-pos-of this) +segment-size+)
+                     +segment-size+ +segment-size+
+                     :fill-paint +food-color+)
   ;; Draw SNAKE
   (mapc #'(lambda (pos)
-            (gamekit:draw-rect pos +segment-size+ +segment-size+
+            (gamekit:draw-rect (gamekit:mult pos +segment-size+)
+                               +segment-size+ +segment-size+
                                :fill-paint +snake-color+))
         (segments-of (player-of this)))
-  ;; Draw food
-  (gamekit:draw-rect (food-pos-of this)
-                     +segment-size+
-                     +segment-size+
-                     :fill-paint +food-color+))
+  ;; Draw SCORE
+  (gamekit:draw-text (format nil "Score: ~A" (score-of this))
+                     (gamekit:vec2 25 (- +screen-height+ 25))))
 
 (defun play ()
   (gamekit:start 'snake-game :viewport-resizable nil))
